8d6320a147a0d7c11f6ed55ca1d72e6a4fd4638a
hanshaoda <hanshaoda@hongyuanshidai.cn>
2017-10-14 16:47:57 +0800

    加入时分秒针

--- app/src/main/java/com/hanshaoda/hanwegidt/MyClockView.java
+++ app/src/main/java/com/hanshaoda/hanwegidt/MyClockView.java
@@ -9,6 +9,10 @@ import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * author: hanshaoda
  * created on: 2017/10/12 下午5:07
@@ -18,19 +22,32 @@ import android.view.View;
 public class MyClockView extends View {
 
     public static final float DEFAULT_BOUNDARY_WIDTH = 7f;//外圆边框宽度
-    public static final float LONG_DEGREE_LENGTH = 30f;
-    public static final float SHORT_DEGREE_LENGTH = 15f;
+    public static final float LONG_DEGREE_LENGTH = 30f;//长刻度线
+    public static final float SHORT_DEGREE_LENGTH = 15f;//短刻度线
+    public static final float DEFAULT_MORE_LENGTH = 35f;//针长处原点的长度
     private Paint mPaintCricle;
     private Paint mPaintOutCri;
     private Paint mPaintHour;
     private Paint mPaintDial;
     private Paint mPaintDegree;
     private Paint mPaintNumber;
-
+    //    刻度盘字体大小
     private int degreeNumberSize = 40;
     private Paint mPaintMinute;
     private Paint mPaintSecond;
     private Paint mPaintCenter;
+    //    秒
+    private int second;
+    //    分
+    private int minute;
+    //    小时
+    private int hour;
+    //    是否刷新时间
+    private boolean isPuase;
+    private float secondLength;
+    private float minuteLength;
+    private float hourLength;
+    private float minutAngle;
 
     public MyClockView(Context context) {
         this(context, null);
@@ -47,6 +64,49 @@ public class MyClockView extends View {
 
     private void init() {
         initPaint();
+        initTime();
+    }
+
+    /**
+     * 初始化时间
+     */
+    private void initTime() {
+        Calendar calendar = Calendar.getInstance();
+        second = calendar.get(Calendar.SECOND);
+        minute = calendar.get(Calendar.MINUTE);
+        hour = calendar.get(Calendar.HOUR_OF_DAY);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (!isPuase) {
+                    postInvalidate();
+                    refreshTime();
+                }
+            }
+        }, 0, 1000);
+    }
+
+    /**
+     * 更新时间
+     */
+    private void refreshTime() {
+
+        second++;
+        if (second == 60) {
+            second = 0;
+            minute++;
+        }
+
+        if (minute == 60) {
+            minute = 0;
+            hour++;
+        }
+
+        if (hour == 24) {
+            hour = 0;
+        }
     }
 
     private void initPaint() {
@@ -107,7 +167,8 @@ public class MyClockView extends View {
         mPaintNumber.setTextSize(degreeNumberSize);
         mPaintNumber.setFakeBoldText(true);
         for (int i = 0; i < 12; i++) {
-
+            float[] temp = setPoints((i + 1) * 30, r - LONG_DEGREE_LENGTH - degreeNumberSize / 2 - 15);
+            canvas.drawText((i + 1) + "", temp[2], temp[3] + degreeNumberSize / 2 - 6, mPaintNumber);
         }
 //        时针
         mPaintHour.setAntiAlias(true);
@@ -120,6 +181,19 @@ public class MyClockView extends View {
         mPaintSecond.setAntiAlias(true);
         mPaintSecond.setStrokeWidth(4);
 
+        secondLength = r * 0.8f;
+        minuteLength = r * 0.6f;
+        hourLength = r * 0.5f;
+
+        minutAngle = minute / 60f * 360;//初始角度
+//        设置时针的起始点
+        float[] hourPoints = setPoints(hour % 12 / 12f * 360 + minute * 5 / 60, hourLength);
+        canvas.drawLine(hourPoints[0], hourPoints[1], hourPoints[2], hourPoints[3], mPaintHour);
+        float[] minutePoints = setPoints(minute / 60f * 360, minuteLength);
+        canvas.drawLine(minutePoints[0], minutePoints[1], minutePoints[2], minutePoints[3], mPaintMinute);
+        float[] secondPoints = setPoints(second / 60f * 360, secondLength);
+        canvas.drawLine(secondPoints[0], secondPoints[1], secondPoints[2], secondPoints[3], mPaintSecond);
+
 
 //        表芯
         mPaintCenter.setColor(Color.GREEN);
@@ -129,5 +203,31 @@ public class MyClockView extends View {
         super.onDraw(canvas);
 
 
+    }
+
+    private float[] setPoints(float v, float length) {
+        float[] floats = new float[4];
+        if (v <= 90) {
+            floats[0] = -(float) (Math.sin(v * Math.PI / 180) * DEFAULT_MORE_LENGTH);
+            floats[1] = (float) (Math.cos(v * Math.PI / 180) * DEFAULT_MORE_LENGTH);
+            floats[2] = (float) (Math.sin(v * Math.PI / 180) * length);
+            floats[3] = -(float) (Math.cos(v * Math.PI / 180) * length);
+        } else if (v <= 180f) {
+            floats[0] = -(float) (Math.cos((v - 90) * Math.PI / 180) * DEFAULT_MORE_LENGTH);
+            floats[1] = -(float) (Math.sin((v - 90) * Math.PI / 180) * DEFAULT_MORE_LENGTH);
+            floats[2] = (float) (Math.cos((v - 90) * Math.PI / 180) * length);
+            floats[3] = (float) (Math.sin((v - 90) * Math.PI / 180) * length);
+        } else if (v <= 270f) {
+            floats[0] = (float) (Math.sin((v - 180) * Math.PI / 180) * DEFAULT_MORE_LENGTH);
+            floats[1] = -(float) (Math.cos((v - 180) * Math.PI / 180) * DEFAULT_MORE_LENGTH);
+            floats[2] = -(float) (Math.sin((v - 180) * Math.PI / 180) * length);
+            floats[3] = (float) (Math.cos((v - 180) * Math.PI / 180) * length);
+        } else if (v <= 360f) {
+            floats[0] = (float) (Math.cos((v - 270) * Math.PI / 180) * DEFAULT_MORE_LENGTH);
+            floats[1] = (float) (Math.sin((v - 270) * Math.PI / 180) * DEFAULT_MORE_LENGTH);
+            floats[2] = -(float) (Math.cos((v - 270) * Math.PI / 180) * length);
+            floats[3] = -(float) (Math.sin((v - 270) * Math.PI / 180) * length);
+        }
+        return floats;
     }
 }
